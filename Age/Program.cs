595a8fd8c6a78923d9796142e11f830d344a4789
4elovel <marcynakvova007@gmail.com>
2023-03-27 18:46:19 +0300

    Добавьте файлы проекта.

---
+++ Age/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+class MyClass
+{
+    static void Main()
+    {
+        Int16 x;
+        Int16 y;
+        Int16 z;
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 1; j < 10; j++)
+            {
+                for(int k = 1; k < 10; k++)
+                {
+                    if (i + j + k == 13 && i * j * k == 36)
+                    {
+                        WriteLine($"First: {i} Second: {j} Third:{k}");
+                    }
+                }
+            }
+        }
+    }
+}
